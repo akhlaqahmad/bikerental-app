8863566c728bf04877c007eb3b27e8e3829dd979
SSA-988 <sujananand0@gmail.com>
2025-09-10 08:07:44 +0530

    first commit

--- store/authStore.tsx
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-const useAuthStore = create((set) => ({
-  user: { phone: '', firstName: '', lastName: '', email: '' },
-  setUser: (userData) => set({ user: userData }),
-}));
-
-export default useAuthStore;
